932abeb8dbdb925d5e97294db11462ca2539bc9c
Superfly Johnson <superfly.johnson@outlook.com>
2022-07-28 13:54:50 -0400

    Created dbconn class

--- dbconn.js
+++ dbconn.js
@@ -3,24 +3,22 @@
 */
 
 // Dependencies
+const fs = require('fs');
+const path = require('path');
 const Pool = require('pg').Pool;
 
 // Configuration
 config = require('./config.js');
 
-// Sets up the pool.
-const pool = new Pool(config.database);
-
-// Tests if the database connection was successful
-pool.connect((err, client, release) => {
-  if (err) {
-    return console.error('Error acquiring client', err.stack)
+// DB Connection Object
+class dbconn {
+  /*
+   * Constructor
+   */
+  #pool = null;
+  constructor(dbinfo){
+    this.#pool = new Pool(dbinfo);
   }
-  client.query('SELECT NOW()', (err, result) => {
-    release()
-    if (err) {
-      return console.error('Error executing query', err.stack)
-    }
-    console.log(result.rows)
-  })
-})
+}
+
+
