e2c431ac0d50ac65355dc53f01bbd82aca5fd40f
Superfly Johnson <superfly.johnson@outlook.com>
2022-07-30 14:39:27 -0400

    Switched configuration format to INI

--- config.js
+++ config.js
@@ -2,7 +2,13 @@
    config.js
    Main configuration file for the application
 */
+const ini = require('ini');
+const fs = require('fs');
 
+cfgPath = './config.ini';
+const config = ini.parse(fs.readFileSync(cfgPath, 'utf-8'));
+
+/*
 const server = {
     port:3000
 };
@@ -19,5 +25,6 @@ const config = {
     server:server,
     database:database
 };
+*/
 
 module.exports = config;
